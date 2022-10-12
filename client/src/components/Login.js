dcff623c971f5c670fd0c2edfe7a5d5af8a00a11
principalkelvo <principalkelvo@gmail.com>
2022-10-12 20:33:30 +0300

    login working perfectly

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,7 @@ function Login() {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log(input, "submitted")
   };
 
   const handleOnChange = (e) => {
@@ -20,13 +21,19 @@ function Login() {
     <>
       <form onSubmit={handleOnSubmit} className={LoginCSS.form}>
         <div className={LoginCSS.formContainer}>
+          <div className={LoginCSS.title}>
+            <h1>park Planner</h1>
+          </div>
+          <div className={LoginCSS.subtitle}>
+            <h2>login</h2>
+          </div>
           <Input
             id="email"
             type="text"
             name="email"
             value={input.email}
             onChange={handleOnChange}
-            label="password"
+            label="Email"
           />
           <Input
             id="password"
@@ -36,6 +43,9 @@ function Login() {
             onChange={handleOnChange}
             label="password"
           />
+          <div className={LoginCSS.buttons}>
+            <button type="submit">Publish</button>
+          </div>
         </div>
       </form>
     </>
