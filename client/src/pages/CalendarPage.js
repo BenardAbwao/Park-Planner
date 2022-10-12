837fd9a9837c99a48d09911f57311d3187ed219c
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-10-13 02:01:58 +0300

    added images

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -4,6 +4,7 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import Bg from "../images/sea-2361247__340.jpg"
 
 const locales = {
     "en-US": require("date-fns/locale/en-US") 
@@ -21,6 +22,7 @@ function CalendarPage({reservations})
 {
     return (
         <>
+            <img className="calendar-bg" src={Bg} alt="bg" />
             <Calendar localizer={localizer} events={reservations} startAccessor="start" endAccessor="end" style={{height: 650, margin: "50px"}} />
         </>
     )
