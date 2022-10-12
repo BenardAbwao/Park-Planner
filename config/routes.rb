0b5ea09237f6e108133da34563079b61fc5657e6
jacksonohallo <jacksonohallo@gmail.com>
2022-10-12 11:53:36 +0300

    add users and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ root 'sessions#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ #users routes
+resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+#sessions routes
+get '/login', to:  'sessions#login'
+get '/login', to:  'sessions#create'
+post '/logout',  to: 'sessions#destroy'
+get '/logout', to:   'sessions#destroy'
 end
