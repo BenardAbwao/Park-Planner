4ea0d17d5130ff52975d0aa99d327f9c517f5ff0
jacksonohallo <jacksonohallo@gmail.com>
2022-10-12 19:54:33 +0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
- root 'sessions#home'
-#users routes
-resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
-#sessions routes
-get '/login', to:  'sessions#login'
-get '/login', to:  'sessions#create'
-post '/logout',  to: 'sessions#destroy'
-get '/logout', to:   'sessions#destroy'
+  #POST/signup
+post '/signup', to: 'users#create'
+#GET /me
+get '/me', to: 'users#show'
 
+#get/index
+get '/users',  to: 'users#index'
+
+#POST /login
+post '/login', to: 'sessions#create'
+
+#DELETE /logout
+
+delete '/logout', to: 'sessions#destroy'
 end
