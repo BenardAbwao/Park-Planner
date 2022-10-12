617ee32f9fa29072b6fb2e50de57e129356089f7
jacksonohallo <jacksonohallo@gmail.com>
2022-10-12 19:59:35 +0300

    update controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
-
 class SessionsController < ApplicationController
-def create
-    @user = User.find_by(username: params[:username])
-    # authenticate user credential
-    if !!@user && user.authenticate(params[:password])
-        # set session and redirect on success
-        session[:user_id] = @user.id
-        redirect_to user_path
-    else
-        message = "something went wrong! Make sure your username and password"
-        redirect_to login_path, notice: message
+
+    #POST /login
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
     end
-   end  
+    
+    #DELETE /logout
+        def destroy
+            session.delete :user_id
+            head :no_content
+        end
 end
