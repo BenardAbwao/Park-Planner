35272887295f62994673b9e47e2c1d6f56d3d7f7
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-13 15:48:23 +0300

    created nested route for park/campsite

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   post '/sign_up', to: "users#create"
   resources :users, except: [:new]
 
-  get '/parks', to: 'parks#index'
-  post '/park', to: 'parks#create'
+  # get '/parks', to: 'parks#index'
+  # post '/park', to: 'parks#create'
+
+  # get '/park[id]', to:'parks#show'
+  get 'parks[:id]/campsites', to: 'campsites#index'
+
+  resources :campsites, only: [:index, :show]
+  resources :parks, only: [:index, :show]
+
 end
