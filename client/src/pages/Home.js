59b5a4a10324e31ac3179d428fa68a243c5c88b9
principalkelvo <principalkelvo@gmail.com>
2022-10-13 14:05:50 +0300

    replaces the entire home structure May lead to merge conflict

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,52 @@
 // import {collage} from '../images/photo-1665510394116-7bd316a1cba5.avif';
-import {  Link } from "react-router-dom";
+// import {  Link } from "react-router-dom";
+import logo from "../../logo.svg";
+import HomeCSS from "../components/home/Home.module.css";
+import Park from "../components/park/ParkCard"
 
-
-function Home({user}) {
-    return (
-        <div className="App">
-            {/* <img className="home-img" src={collage} alt={photo}/> */}
-            <div className="welcome">
-                <p> welcome to your National Park Planner!</p>
-            </div>
-            <Link to="/parks-page">
-                <div className="begin-exploring">
-                    <div className="overlay-text">
-                        <h1>Begin Exploring. . .</h1>
-                    </div>
-                </div>
-            </Link>
+function Home({ user }) {
+  return (
+    <>
+      <div className={HomeCSS.home}>
+        {/* hero image*/}
+        <div className={HomeCSS.hero}>
+          <img src={logo} alt="#" />
         </div>
-    )
-}
+        {/* search bar  */}
 
+        {/* about us*/}
+        <div className={HomeCSS.about}>
+          <div className={HomeCSS.icons}>
+            <div className={HomeCSS.icon}>
+              <p>relax</p>
+            </div>
+            <div className={HomeCSS.icon}>
+              <p>Cultural</p>
+            </div>
+            <div className={HomeCSS.icon}>
+              <p>Beach</p>
+            </div>
+            <div className={HomeCSS.icon}>
+              <p>Historical</p>
+            </div>
+          </div>
+          <div className={HomeCSS.ishalf}>
+            <p>
+              Our specialty is the ability to design unique trips and safaris to
+              fit every type of tourist,providing a unique opportunity to
+              combine bush and beach adventures in hidden treasures and
+              spectacular sceneries of the world
+            </p>
+          </div>
+        </div>
+        {/* popular trips  */}
+        <div className={HomeCSS.popular}>
+            <h1>Most <>Popular</> trips</h1>
+            <Park/>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Home;
