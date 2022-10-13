a943494f1d097932fb8820c88813f6d3ee2f8e62
principalkelvo <principalkelvo@gmail.com>
2022-10-14 00:41:44 +0300

    all parks page

--- client/src/pages/Parks.js
+++ client/src/pages/Parks.js
@@ -1,6 +1,7 @@
 import ParkCard from "../components/park/ParkCard";
 
-function Parks() {
-  return <ParkCard />;
+function Parks({parks}) {
+
+  return <ParkCard parks={parks} />;
 }
 export default Parks;
