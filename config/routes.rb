10411da29cf9f68b6ab5a1aecc9251c5b974fafd
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-13 14:31:24 +0300

    Create route for displaying all parks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "users#index"
-  # get '/users', to: "users#index"
-  # get '/sign_up', to: "users#new" 
+  
   post '/sign_up', to: "users#create"
   resources :users, except: [:new]
+
+  get '/parks', to: 'parks#index'
+  post '/park', to: 'parks#create'
 end
