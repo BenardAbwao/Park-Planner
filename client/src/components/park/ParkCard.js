27d7d536c2eb47797bfd61dcad8db13077e581cb
principalkelvo <principalkelvo@gmail.com>
2022-10-14 01:13:28 +0300

    check for parks length before showing items

--- client/src/components/park/ParkCard.js
+++ client/src/components/park/ParkCard.js
@@ -4,32 +4,32 @@ function ParkCard({ parks }) {
   return (
     <>
       <div className={ParkCardCSS.fullCard}>
-          {parks.length > 0 ? (
-            parks.map((park) => (
-              <div className={ParkCardCSS.card} key={park.id}>
-                {/* image */}
-                <div className={ParkCardCSS.image}>
-                  <img src={park.image} alt={park.name} />
-                </div>
-                {/* title */}
-                <div className={ParkCardCSS.title}>
-                  <h2>{park.name}</h2>
-                </div>
-                {/* rate */}
-                <div className={ParkCardCSS.rate}>
-                  <p>rate: 2</p>
-                </div>
-                <div className={ParkCardCSS.buttons}>
-                  <button>Book Now</button>
-                </div>
+        {parks.length > 0 ? (
+          parks.map((park) => (
+            <div className={ParkCardCSS.card} key={park.id}>
+              {/* image */}
+              <div className={ParkCardCSS.image}>
+                <img src={park.image} alt={park.name} />
               </div>
-            ))
-          ) : (
-            <>
-              <h2>No Parks Found</h2>
-            </>
-          )}
-        </div>
+              {/* title */}
+              <div className={ParkCardCSS.title}>
+                <h2>{park.name}</h2>
+              </div>
+              {/* rate */}
+              <div className={ParkCardCSS.rate}>
+                <p>rate: 2</p>
+              </div>
+              <div className={ParkCardCSS.buttons}>
+                <button>Book Now</button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <>
+            <h2>No Parks Found</h2>
+          </>
+        )}
+      </div>
     </>
   );
 }
