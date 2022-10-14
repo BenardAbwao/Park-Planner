d94c8f4fa1d216bc408d6a0a3a192754da211342
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-14 10:09:10 +0300

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index
         users = User.all
         render json: users
@@ -10,25 +9,15 @@ class UsersController < ApplicationController
     end
       
     def create
-
         @user = User.create!(create_params)
-        # user = User.create!(create_params)
         render json: @user
-
-        # if @user.valid?
-        #     app_response(status_code: 201, message: "Account Successfully Created", body: user)
-
-        # else
-        #     app_response(status_code: 422, message: "Invalid input", body: user.errors.full_messages)
-
-        # end
-
     end
 
     private
-    
+
     def create_params
         params.permit(:username, :email, :password)
     end
 
 end
+ 
